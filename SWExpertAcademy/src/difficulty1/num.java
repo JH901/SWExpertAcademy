2de6c4660b4392c7036a97cdf83d51045d6b7078
JiHyunJang <jhJang88@lotte.net>
2019-10-22 16:39:58 +0900

    20191022 SWExpertAcademy 잔디심기 시작

---
+++ SWExpertAcademy/src/difficulty1/num.java
@@ -0,0 +1,11 @@
+package difficulty1;
+import java.util.*;
+
+public class num {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ SWExpertAcademy/src/difficulty1/num8741.java
@@ -0,0 +1,28 @@
+package difficulty1;
+import java.util.*;
+
+public class num8741 {
+
+	public static void main(String[] args) {
+		Scanner scan  = new Scanner(System.in);
+		int tc = scan.nextInt();
+		scan.nextLine();
+		
+		String[] results = new String[tc];
+		for(int i=0; i<tc; i++) {
+			String input = scan.nextLine();
+			
+			String[] splitInput = input.split(" ");
+			String result = "";
+			for(int j=0; j<splitInput.length; j++) {
+				result += splitInput[j].toUpperCase().charAt(0);
+			}
+			results[i] = result;
+		}
+		
+		for(int i=0; i<tc; i++){
+			System.out.println("#"+(i+1)+" "+results[i]);
+		}
+	}
+
+}
