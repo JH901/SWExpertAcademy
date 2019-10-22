d5d753081a246904c598788fdc24b51ab42e9f9f
JiHyunJang <jhJang88@lotte.net>
2019-10-22 17:24:32 +0900

    D1 2046. 스탬프 찍기

---
+++ SWExpertAcademy/src/D1/num2046.java
@@ -0,0 +1,17 @@
+package D1;
+
+import java.util.*;
+
+public class num2046 {
+
+	public static void main(String[] args) {
+		Scanner scan  = new Scanner(System.in);
+		int num = scan.nextInt();
+		
+		for(int i=0; i<num; i++) {
+			System.out.print("#");
+		}
+
+	}
+
+}
