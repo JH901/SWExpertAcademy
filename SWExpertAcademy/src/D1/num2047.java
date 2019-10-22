91382be09bacc521978e85eedfed5ab3ef950c0e
JiHyunJang <jhJang88@lotte.net>
2019-10-22 17:22:32 +0900

    D1 2047. 신문 헤드라인

---
+++ SWExpertAcademy/src/D1/num2047.java
@@ -0,0 +1,15 @@
+package D1;
+
+import java.util.*;
+
+public class num2047 {
+
+	public static void main(String[] args) {
+		Scanner scan  = new Scanner(System.in);
+		String str = scan.nextLine();
+		
+		System.out.println(str.toUpperCase());
+
+	}
+
+}
