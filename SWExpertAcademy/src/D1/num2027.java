02bb568dffc1bb9662b712f5bd5653d5fec0c5a8
JiHyunJang <jhJang88@lotte.net>
2019-10-23 09:34:13 +0900

    2027. 대각선 출력하기

---
+++ SWExpertAcademy/src/D1/num2027.java
@@ -0,0 +1,20 @@
+package D1;
+
+import java.util.*;
+
+public class num2027 {
+
+	public static void main(String[] args) {
+		Scanner scan  = new Scanner(System.in);
+		
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<5; j++) {
+				if(i==j) System.out.print("#");
+				else System.out.print("+");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
