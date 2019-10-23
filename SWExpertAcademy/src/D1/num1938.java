39523e54939c63dbb9b41ab7e3b8fb2c56152cac
JiHyunJang <jhJang88@lotte.net>
2019-10-23 09:39:15 +0900

    1938. 아주 간단한 계산기

---
+++ SWExpertAcademy/src/D1/num1938.java
@@ -0,0 +1,19 @@
+package D1;
+
+import java.util.*;
+
+public class num1938 {
+
+	public static void main(String[] args) {
+		Scanner scan  = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+
+	}
+
+}
